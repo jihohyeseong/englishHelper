435899a42f2230c0f5372e67b08de3f64a320fe4
jihohyeseong <hsl4720@naver.com>
2025-03-07 23:46:35 +0900

    valid change

--- src/main/java/com/example/englishHelper/dto/UserDto.java
+++ src/main/java/com/example/englishHelper/dto/UserDto.java
@@ -18,12 +18,12 @@ public class UserDto {
 
     private Long id;
 
-    @NotEmpty(message = "id는 최소 3자 이상 최대 20자 이하여야 합니다.")
-    @Size(min = 3 , max = 20, message = "id는 최소 3자 이상 최대 20자 이하여야 합니다.")
+    @NotEmpty(message = "id는 최소 5자 이상 최대 20자 이하여야 합니다.")
+    @Size(min = 5 , max = 20, message = "id는 최소 5자 이상 최대 20자 이하여야 합니다.")
     private String username;
 
-    @NotEmpty(message = "비밀번호는 최소 5자 이상이어야 하며, 특수문자 하나 이상을 포함해야 합니다.")
-    @Pattern(regexp = "^(?=.*[!@#$%^&*()-+=])(?=\\S+$).{5,}$", message = "비밀번호는 최소 5자 이상이어야 하며, 특수문자 하나 이상을 포함해야 합니다.")
+    @NotEmpty(message = "비밀번호는 최소 8자 이상이어야 하며, 특수문자 하나 이상을 포함해야 합니다.")
+    @Pattern(regexp = "^(?=.*[!@#$%^&*()-+=])(?=\\S+$).{8,}$", message = "비밀번호는 최소 8자 이상이어야 하며, 특수문자 하나 이상을 포함해야 합니다.")
     private String password;
 
     @Email(message = "이메일 형식이 올바르지 않습니다.")
