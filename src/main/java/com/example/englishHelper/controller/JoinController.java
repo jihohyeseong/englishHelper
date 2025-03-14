b6529b62252a9a6b050739d854b87dea99c811c0
jihohyeseong <hsl4720@naver.com>
2025-03-14 23:31:05 +0900

    eureka client apply

--- src/main/java/com/example/englishHelper/controller/JoinController.java
+++ src/main/java/com/example/englishHelper/controller/JoinController.java
@@ -10,12 +10,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/auth")
 public class JoinController {
 
     private final JoinService joinService;
--- src/main/java/com/example/englishHelper/controller/ReissueController.java
+++ src/main/java/com/example/englishHelper/controller/ReissueController.java
@@ -6,9 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class ReissueController {
 
     private final ReissueService reissueService;
